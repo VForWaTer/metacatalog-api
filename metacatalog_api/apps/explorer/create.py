e035d87d1ca5e051ab9c71b5a99c5b09196dc0a2
Mirko Mälicke <mirko@hydrocode.de>
2024-12-29 13:38:36 +0100

    fix paths

--- metacatalog_api/apps/explorer/create.py
+++ metacatalog_api/apps/explorer/create.py
@@ -8,7 +8,7 @@ from metacatalog_api.server import server
 create_router = APIRouter()
 templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
 
-@create_router.get('/entries/new.html')
+@create_router.get('/create/entries.html')
 def new_entry_page(request: Request):
     return templates.TemplateResponse(request=request, name="add_entry.html", context={"path": server.uri_prefix})
 
@@ -21,17 +21,17 @@ def leaflet_draw(request: Request, geom: str = 'marker'):
         return templates.TemplateResponse(request=request, name="leaflet_extent.html", context={})
 
 
-@create_router.get('/authors/new.html')
+@create_router.get('/create/authors.html')
 def new_author(request: Request):
     return templates.TemplateResponse(request=request, name="author.html", context={"path": server.uri_prefix})
 
 
-@create_router.get('/details/new.html')
+@create_router.get('/create/details.html')
 def new_details(request: Request):
     return templates.TemplateResponse(request=request, name="details.html", context={"path": server.uri_prefix})
 
 
-@create_router.get('/datasources/new.html')
+@create_router.get('/create/datasources.html')
 def new_datasource(request: Request):
     # load the datasource types
     types = core.datatypes()
