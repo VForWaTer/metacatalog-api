0a83f8fa6ba700adebe8516860f6756ce42acf69
Mirko Mälicke <mirko@hydrocode.de>
2025-01-07 19:40:54 +0100

    fix create paths

--- metacatalog_api/router/api/create.py
+++ metacatalog_api/router/api/create.py
@@ -16,7 +16,7 @@ def add_datasource(entry_id: int, payload: models.DatasourceCreate) -> models.Me
     metadata = core.add_datasource(entry_id=entry_id, payload=payload)
     return metadata
 
-@create_router.post('/author')
+@create_router.post('/authors')
 def add_author(payload: models.AuthorCreate, no_duplicates: bool = True) -> models.Author:
     """
     Create a new Author. If the author already exists, the existing author is returned.
@@ -25,7 +25,7 @@ def add_author(payload: models.AuthorCreate, no_duplicates: bool = True) -> mode
     author = core.add_author(payload, no_duplicates=no_duplicates)
     return author
 
-@create_router.post('/group')
+@create_router.post('/groups')
 def add_group(payload: models.EntryGroupCreate) -> models.EntryGroup:
     group = core.add_group(payload=payload)
     return group
