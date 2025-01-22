24be7bca6a237d165aba9d6b1a6efbc65cb2de68
Mirko Mälicke <mirko.maelicke@kit.edu>
2025-01-22 10:11:14 +0100

    add new migration

--- metacatalog_api/sql/maintain/install.sql
+++ metacatalog_api/sql/maintain/install.sql
@@ -502,3 +502,14 @@ CREATE TABLE {schema}.metacatalog_info
     max_py_version character varying(64),
     CONSTRAINT metacatalog_info_pkey PRIMARY KEY (db_version)
 );
+
+CREATE TABLE IF NOT EXISTS user_access_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    token_hash CHARACTER VARYING (64) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    valid_until TIMESTAMP,
+    CONSTRAINT persons_access_token
+        FOREIGN KEY (user_id) REFERENCES persons (id) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
